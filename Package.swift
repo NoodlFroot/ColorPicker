7d94be81dbb3444cf210ef67c3879016f1eb8b1b
Richard Groves <richard@noodlfroot.com>
2020-05-16 14:27:01 +0100

    Update the DynamicColor fork version we need
    Get the one with update iOS minimum version in package file

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 		// RAG: Fixing the release build problem using my own fork
-        .package(url: "https://github.com/NoodlFroot/DynamicColor", from: "5.0.1")
+        .package(url: "https://github.com/NoodlFroot/DynamicColor", from: "5.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
